f33aaced8b05488107a0c71e4ee71e4aa413a570
Harsh Patel <hpatel1@iuw.edu>
2026-02-02 22:08:31 -0600

    Added a filtering mechanism to filter by department

--- csv_parser.py
+++ csv_parser.py
@@ -4,9 +4,12 @@ def main():
     print("CSV Parser")
     # My code goes here
     with open('employees.csv', newline='') as csvfile:
+        next(csvfile)
         reader = csv.reader(csvfile)
         for row in reader:
             print(row)
+        
+
     # File automatically closes here, even if there's an error, 
     # the with statement closes the file without manually doing it,
     # Manual way you have to close it but if something crashes before
@@ -14,6 +17,16 @@ def main():
 
     # TLDR: 'with' statement lets me open a file, use it, and AUTOMATICALLY close
     # It when the block ends, kinda like autosave on a video game
+
+    # Part 2 Make your CSV parser ask the user which department they want to see, then only show employees from that department.
+    user_input = input("Which department do you want to filter by?")
+    with open('employees.csv', newline='') as c:
+        reader = csv.reader(c)
+        for row in reader:
+            if row[2].lower() == user_input.lower():
+                print(row)
+    
+
 
 
 
